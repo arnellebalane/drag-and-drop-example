562da4088307500bfbad7a8373fa64911376b14c
Arnelle Balane <arnellebalane@gmail.com>
2018-06-03 19:58:26 +0800

    Add styles for different drag states

--- index.js
+++ index.js
@@ -24,15 +24,36 @@ html.addEventListener('dragstart', e => {
     e.dataTransfer.setData('text/plain', html.textContent);
 });
 
+[text, link, image, html].forEach(draggable => {
+    draggable.addEventListener('dragstart', e => {
+        e.target.classList.add('dragging');
+    });
+
+    draggable.addEventListener('dragend', e => {
+        e.target.classList.remove('dragging');
+    });
+});
+
 
 // Dropzone functionality
 const dropzone = document.querySelector('.dropzone');
+
+dropzone.addEventListener('dragenter', e => {
+    e.target.classList.add('dragover');
+});
+
+dropzone.addEventListener('dragleave', e => {
+    e.target.classList.remove('dragover');
+});
 
 dropzone.addEventListener('dragover', e => {
     e.preventDefault();
 });
 
 dropzone.addEventListener('drop', e => {
+    e.preventDefault();
+    e.target.classList.remove('dragover');
+
     const transfer = e.dataTransfer;
 
     for (let i = 0; i < transfer.items.length; i++) {
